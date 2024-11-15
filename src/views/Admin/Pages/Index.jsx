0eec801cffbbca6ace95889920b0bd378b4f1b6b
Dwidy99 <98dwiyulianto@gmail.com>
2024-11-15 16:37:40 +0700

    CRUD Data Pages : Show Data Pages

--- src/views/Admin/Pages/Index.jsx
+++ src/views/Admin/Pages/Index.jsx
@@ -1,10 +1,194 @@
+//import react
+import { useEffect, useState } from "react";
+//import react-router-dom
+import { Link } from "react-router-dom";
+
 //import LayoutAdmin
 import LayoutAdmin from "../../../layouts/Admin";
 
+//import Api
+import Api from "../../../services/Api";
+//import Pagination
+import Pagination from "../../../components/general/Pagination";
+//import hasAnyPermission
+import hasAnyPermissions from "../../../utils/Permissions";
+
+//improt Cookies js
+import Cookies from "js-cookie";
+
 export default function PagesIndex() {
+  //title Page
+  document.title = "Pages - Desa Digital";
+
+  //define pages state
+  const [pages, setPages] = useState([]);
+
+  //define Pagination
+  const [pagination, setPagination] = useState({
+    currentPage: 0,
+    perPage: 0,
+    total: 0,
+  });
+
+  //define keywords state
+  const [keywords, setKeywords] = useState("");
+
+  //token
+  const token = Cookies.get("token");
+
+  //function "fetchData"
+  const fetchData = async (pageNumber = 1, keywords = "") => {
+    //define variabel "page"
+    const page = pageNumber ? pageNumber : pagination.currentPage;
+
+    await Api.get(`/api/admin/pages?search=${keywords}&page=${page}`, {
+      //headers
+      headers: {
+        //headers + token
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((response) => {
+      //set data response to state "setPages"
+      setPages(response.data.data.data);
+
+      //set data pagination to state "pagination"
+      setPagination(() => ({
+        currentPage: response.data.data.current_page,
+        perPage: response.data.data.per_page,
+        total: response.data.data.total,
+      }));
+    });
+  };
+
+  //useEffect
+  useEffect(() => {
+    //call function "fetchData"
+    fetchData();
+  }, []);
+
+  //function "search Data"
+  const searchData = async (e) => {
+    //set value to state "keyword"
+    setKeywords(e.target.value);
+
+    //call method "fetchdate"
+    fetchData(1, e.target.value);
+  };
+
   return (
     <LayoutAdmin>
-      <h1>PagesIndex</h1>
+      <main>
+        <div className="container-fluid px-4 mt-5">
+          <div className="row">
+            <div className="col-md-9">
+              <div className="row">
+                {hasAnyPermissions(["pages.create"]) && (
+                  <div className="col-md-3 col-12 mb-2">
+                    <Link
+                      to="/admin/pages/create"
+                      className="btn btn-primary border-0 shadow-sm w-100"
+                      type="button"
+                    >
+                      <i className="fa fa-plus-circle"></i> Add New
+                    </Link>
+                  </div>
+                )}
+
+                <div className="col-md-9 col-12 mb-2">
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      className="form-control border-0 shadow-sm"
+                      onChange={(e) => searchData(e)}
+                      placeholder="search here.."
+                    />
+                    <span className="input-group-text border-0 shadow-sm">
+                      <i className="fa fa-search"></i>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row mt-1">
+            <div className="col-md-12">
+              <div className="card border-0 rounded shadow-sm border-sm border-top-success">
+                <div className="card-body">
+                  <div className="table-responsive">
+                    <table className="table table-bordered table-centered table-nowrap mb-0 rounded">
+                      <thead className="thead-dark">
+                        <tr className="border-0">
+                          <th className="border-0" style={{ width: "5%" }}>
+                            No.
+                          </th>
+                          <th className="border-0">Title</th>
+                          <th className="border-0" style={{ width: "15%" }}>
+                            Actions
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {
+                          //cek apakah data ada?
+                          pages.length > 0 ? (
+                            //looping data "pages" dengan "map"
+                            pages.map((page, index) => (
+                              <tr key={index}>
+                                <td className="fw-bold text-center">
+                                  {++index +
+                                    (pagination.currentPage - 1) *
+                                      pagination.perPage}
+                                </td>
+                                <td>{page.title}</td>
+                                <td className="text-center">
+                                  {hasAnyPermissions(["pages.edit"]) && (
+                                    <Link
+                                      to={`/admin/pages/edit/${page.id}`}
+                                      className="btn btn-warning btn-sm me-2"
+                                    >
+                                      <i className="fa fa-pencil-alt"></i>
+                                    </Link>
+                                  )}
+
+                                  {hasAnyPermissions(["pages.delete"]) && (
+                                    <button className="btn btn-danger btn-sm me-2">
+                                      <i className="fa fa-trash"></i>
+                                    </button>
+                                  )}
+                                </td>
+                              </tr>
+                            ))
+                          ) : (
+                            //tampilan pesan data belum tersedia
+                            <tr>
+                              <td colSpan={5}>
+                                <div
+                                  className="alert alert-danger border-0 rounded shadow-sm w-100 text-center"
+                                  role="alert"
+                                >
+                                  Data Not Availabel..
+                                </div>
+                              </td>
+                            </tr>
+                          )
+                        }
+                      </tbody>
+                    </table>
+                  </div>
+
+                  <Pagination
+                    currentPage={pagination.currentPage}
+                    perPage={pagination.perPage}
+                    total={pagination.total}
+                    onChange={(pageNumber) => fetchData(pageNumber, keywords)}
+                    position="end"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </LayoutAdmin>
   );
 }
