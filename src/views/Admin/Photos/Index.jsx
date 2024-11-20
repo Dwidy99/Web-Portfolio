1d81c343bd3a4735c88c21038f1a5be8acb2fdcf
Dwidy99 <98dwiyulianto@gmail.com>
2024-11-21 00:11:44 +0700

    CRUD Data Photos : Show Data Photos

--- src/views/Admin/Photos/Index.jsx
+++ src/views/Admin/Photos/Index.jsx
@@ -1,10 +1,199 @@
+//import react
+import { useEffect, useState } from "react";
+//import react-router-dom
+import { Link } from "react-router-dom";
+
 //import LayoutAdmin
 import LayoutAdmin from "../../../layouts/Admin";
 
+//import Api
+import Api from "../../../services/Api";
+//import Pagination
+import Pagination from "../../../components/general/Pagination";
+
+//import Cookies js
+import Cookies from "js-cookie";
+import hasAnyPermissions from "../../../utils/Permissions";
+
 export default function PhotosIndex() {
+  //page title
+  document.title = "Photos - Desa Digital";
+
+  //define state "photos"
+  const [photos, setPhotos] = useState([]);
+
+  //define state "pagination"
+  const [pagination, setPagination] = useState({
+    currentPage: 0,
+    perPage: 0,
+    total: 0,
+  });
+
+  //define state "keywords"
+  const [keywords, setKeywords] = useState("");
+
+  //token from Cookies
+  const token = Cookies.get("token");
+
+  //finction "fetchData"
+  const fetchData = async (pageNumber = 1, keywords = "") => {
+    const page = pageNumber ? pageNumber : pagination.currentPage;
+    await Api.get(`/api/admin/photos?search${keywords}&page=${page}`, {
+      //header
+      headers: {
+        //header + token
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((response) => {
+      //set data response to state "setPosts"
+      setPhotos(response.data.data.data);
+
+      //set data response to state "pagination"
+      setPagination(() => ({
+        currentPage: response.data.data.current_page,
+        perPage: response.data.data.per_page,
+        total: response.data.data.total,
+      }));
+    });
+  };
+
+  //useEffect
+  useEffect(() => {
+    //call "fetchData"
+    fetchData();
+  }, []);
+
+  //function "searchData"
+  const searchData = async (e) => {
+    //set value to state "keywords"
+    setKeywords(e.target.value);
+
+    //call function "fetchData"
+    fetchData(1, e.target.value);
+  };
+
   return (
     <LayoutAdmin>
-      <h1>SlidersIndex</h1>
+      <main>
+        <div className="container-fluid mb-5 mt-5">
+          <div className="row">
+            <div className="col-md-8">
+              <div className="row">
+                {hasAnyPermissions(["posts.create"]) && (
+                  <div className="col-md-3 col-12 mb-12">
+                    <Link
+                      to="/admin/posts/create"
+                      className="btn btn-md btn-primary border-primary border-0 shadow w-100"
+                      type="button"
+                    >
+                      <i className="fa fa-plus-circle"></i> Add new
+                    </Link>
+                  </div>
+                )}
+
+                <div className="col-md-9 col-12 mb-2">
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      onChange={(e) => searchData(e)}
+                      placeholder="search here.."
+                    />
+                    <span className="input-group-text border-0 shadow-sm">
+                      <i className="fa fa-search"></i>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row mt-1">
+            <div className="col-md-12">
+              <div className="card border-0 rounded shadow-sm border-sm border-top-success">
+                <div className="card-body">
+                  <div className="table-responsive">
+                    <table className="table table-bordered table-centered table-nowrap mb-0 rounded">
+                      <thead className="thead-dark">
+                        <tr className="border-0">
+                          <th className="border-0" style={{ width: "5%" }}>
+                            No.
+                          </th>
+                          <th className="border-0">Image</th>
+                          <th className="border-0">Caption</th>
+                          <th className="border-0" style={{ width: "15%" }}>
+                            Actions
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {
+                          //cek apakah data ada?
+                          photos.length > 0 ? (
+                            //looping data "photos" dengan "map"
+                            photos.map((photo, index) => (
+                              <tr key={index}>
+                                <td className="fw-bold text-center">
+                                  {++index +
+                                    (pagination.currentPage - 1) *
+                                      pagination.perPage}
+                                </td>
+                                <td className="text-center">
+                                  <img
+                                    src={photo.image}
+                                    width={"150px"}
+                                    className="rounded"
+                                  />
+                                </td>
+                                <td>{photo.caption}</td>
+                                <td className="text-center">
+                                  {hasAnyPermissions(["photos.edit"]) && (
+                                    <Link
+                                      to={`/admin/photos/edit/${photo.id}`}
+                                      className="btn btn-primary btn-sm me-2"
+                                    >
+                                      <i className="fa fa-pencil-alt"></i>
+                                    </Link>
+                                  )}
+
+                                  {hasAnyPermissions(["photos.delete"]) && (
+                                    <button className="btn btn-danger btn-sm">
+                                      <i className="fa fa-trash"></i>
+                                    </button>
+                                  )}
+                                </td>
+                              </tr>
+                            ))
+                          ) : (
+                            //tampilan pesan data belum tersedia
+                            <tr>
+                              <td colSpan={4}>
+                                <div
+                                  className="alert alert-danger border-0 rounded shadow-sm w-100 text-center"
+                                  role="alert"
+                                >
+                                  Data Not Available..
+                                </div>
+                              </td>
+                            </tr>
+                          )
+                        }
+                      </tbody>
+                    </table>
+                  </div>
+
+                  <Pagination
+                    currentPage={pagination.currentPage}
+                    perPage={pagination.perPage}
+                    total={pagination.total}
+                    onChange={(pageNumber) => fetchData(pageNumber, keywords)}
+                    position="end"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </LayoutAdmin>
   );
 }
