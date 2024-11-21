143692d1a93eba6b32eaa3ff60d66ce0ff87ea47
Dwidy99 <98dwiyulianto@gmail.com>
2024-11-21 15:25:48 +0700

    CRUD Data Sliders : Show Data Sliders

--- src/views/Admin/Sliders/Index.jsx
+++ src/views/Admin/Sliders/Index.jsx
@@ -1,10 +1,157 @@
+//import react
+import { useEffect, useState } from "react";
+//import react-router-dom
+import { Link } from "react-router-dom";
+
 //import LayoutAdmin
 import LayoutAdmin from "../../../layouts/Admin";
 
+//import Api
+import Api from "../../../services/Api";
+//import Pagination
+import Pagination from "../../../components/general/Pagination";
+
+//import cookies js
+import Cookies from "js-cookie";
+//import hasAnyPermission
+import hasAnyPermissions from "../../../utils/Permissions";
+
 export default function SlidersIndex() {
+  //page title
+  document.title = "Slider - Desa Digital";
+
+  //define state "sliders"
+  const [sliders, setSliders] = useState("");
+
+  //define state "Pagination"
+  const [pagination, setPagination] = useState({
+    currentPage: 0,
+    perPage: 0,
+    total: 0,
+  });
+
+  //token from cookies
+  const token = Cookies.get("token");
+
+  //function fetchData
+  const fetchData = async (pageNumber = 1) => {
+    //define variabel "page"
+    const page = pageNumber ? pageNumber : pagination.currentPage;
+
+    await Api.get(`/api/admin/sliders?page=${page}`, {
+      //header
+      headers: {
+        //header + token
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((response) => {
+      //set data response to state "setSliders"
+      setSliders(response.data.data.data);
+
+      //set data response to state "pagination"
+      setPagination(() => ({
+        currentPage: response.data.data.current_page,
+        perPage: response.data.data.per_page,
+        total: response.data.data.total,
+      }));
+    });
+  };
+
+  //useEffect
+  useEffect(() => {
+    //call funtion fetchData
+    fetchData();
+  }, []);
+
   return (
     <LayoutAdmin>
-      <h1>SlidersIndex</h1>
+      <main>
+        <div className="container-fluid mb-5 mt-5">
+          <div className="row mt-1">
+            <div className="col-md-12">
+              <div className="card border-0 rounded shadow-sm border-sm border-top-success">
+                <div className="card-body">
+                  <div className="table-responsive">
+                    <table className="table table-bordered table-centered table-nowrap mb-0 rounded">
+                      <thead className="thead-dark">
+                        <tr className="border-0">
+                          <th className="border-0" style={{ width: "5%" }}>
+                            No.
+                          </th>
+                          <th className="border-0">Image</th>
+                          <th className="border-0" style={{ width: "15%" }}>
+                            Actions
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {
+                          //cek apakah data ada?
+                          sliders.length > 0 ? (
+                            //looping data "sliders" dengan "map"
+                            sliders.map((slider, index) => (
+                              <tr key={index}>
+                                <td className="fw-bold text-center">
+                                  {++index +
+                                    (pagination.currentPage - 1) *
+                                      pagination.perPage}
+                                </td>
+                                <td className="text-center">
+                                  <img
+                                    src={slider.image}
+                                    width={"100px"}
+                                    className="rounded"
+                                  />
+                                </td>
+                                <td className="text-center">
+                                  {hasAnyPermissions(["sliders.edit"]) && (
+                                    <Link
+                                      to={`/admin/sliders/edit/${slider.id}`}
+                                      className="btn btn-primary btn-sm me-2"
+                                    >
+                                      <i className="fa fa-pencil-alt"></i>
+                                    </Link>
+                                  )}
+
+                                  {hasAnyPermissions(["sliders.delete"]) && (
+                                    <button className="btn btn-danger btn-sm">
+                                      <i className="fa fa-trash"></i>
+                                    </button>
+                                  )}
+                                </td>
+                              </tr>
+                            ))
+                          ) : (
+                            //tampilan pesan data belum tersedia
+                            <tr>
+                              <td colSpan={4}>
+                                <div
+                                  className="alert alert-danger border-0 rounded shadow-sm w-100 text-center"
+                                  role="alert"
+                                >
+                                  Data Not Available..
+                                </div>
+                              </td>
+                            </tr>
+                          )
+                        }
+                      </tbody>
+                    </table>
+                  </div>
+
+                  <Pagination
+                    currentPage={pagination.currentPage}
+                    perPage={pagination.perPage}
+                    total={pagination.total}
+                    onChange={(pageNumber) => fetchData(pageNumber, keywords)}
+                    position="end"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </LayoutAdmin>
   );
 }
