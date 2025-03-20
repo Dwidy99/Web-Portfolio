00064fc155150921a150f5706e7a6a9c89d74b30
Dwidy99 <98dwiyulianto@gmail.com>
2025-03-20 21:20:08 +0700

    SnowEffect : Fix bug SnowEffect

--- src/layouts/Web.jsx
+++ src/layouts/Web.jsx
@@ -7,11 +7,14 @@ import "../assets/web/js/script.js";
 
 import Navbar from "../components/web/Navbar.jsx";
 import Footer from "../components/web/Footer.jsx";
+import SnowEffect from "../components/general/SnowEffect.jsx";
 
 export default function Web({ children }) {
   return (
     <>
       <Navbar />
+      {/* Gunakan komponen SnowEffect di sini */}
+      <SnowEffect />
       {children ? children : <p>Children are missing</p>}
       <Footer />
     </>
