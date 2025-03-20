b6e0f7f6f0fbbe773ba49470dc2cb027dff7f9af
Dwidy99 <98dwiyulianto@gmail.com>
2025-03-20 21:16:50 +0700

    Footer : Improment Component Footer

--- src/components/web/Footer.jsx
+++ src/components/web/Footer.jsx
@@ -4,9 +4,9 @@ export default function Footer() {
   return (
     <footer>
       <div className="container">
-        <div className="mx-16 mb-8.5 mt-16 items-center justify-between space-y-4 md:mb-10 md:flex md:space-y-0">
+        <hr className="mb-8.5 border-slate-300 dark:border-slate-700 my-4" />
+        <div className="mx-16 mb-8.5 items-center justify-between space-y-4 md:mb-10 md:flex md:space-y-0">
           <BuildWith />
-
           <div className="my-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
             <div>{`Copyright © ${new Date().getFullYear()}`}</div>
             <span>{` • `}</span>
