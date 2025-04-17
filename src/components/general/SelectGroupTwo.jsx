a330800f4ec4d03491ca1b0ad5dd24fc083968ce
Dwi9 <98dwiyulianto@gmail.com>
2025-04-17 17:36:53 +0700

    SelectGroupTwo : Add margin Left 4 TailwindCss

--- src/components/general/SelectGroupTwo.jsx
+++ src/components/general/SelectGroupTwo.jsx
@@ -28,7 +28,7 @@ const SelectGroupTwo = ({ value, onChange, options, placeholder }) => {
         <select
           value={value}
           onChange={handleChange}
-          className={`relative z-20 w-full appearance-none rounded border border-stroke bg-transparent ml-4 py-3 px-12 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input ${isOptionSelected ? "text-black dark:text-white" : ""}`}
+          className={`relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-12 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input ${isOptionSelected ? "text-black dark:text-white" : ""}`}
         >
           <option value="" disabled className="text-body dark:text-bodydark">
             {placeholder}
