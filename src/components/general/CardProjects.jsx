9e18d0dff4243c32df1bcee18ffe6fd7ffdc1660
Dwi9 <98dwiyulianto@gmail.com>
2025-05-08 00:34:16 +0700

    CardProjects Components : Create New Component

--- src/components/general/CardProjects.jsx
+++ src/components/general/CardProjects.jsx
@@ -1,6 +1,13 @@
 import PropTypes from "prop-types";
+import DOMPurify from "dompurify";
 
-export default function CardProjects({ image, caption, children }) {
+export default function CardProjects({
+  title,
+  description,
+  image,
+  caption,
+  children,
+}) {
   return (
     <div className="bg-gray-100 border border-gray-300 rounded-2xl shadow-sm overflow-hidden hover:shadow-md transition-shadow duration-300 flex flex-col h-full">
       {image && (
@@ -11,8 +18,24 @@ export default function CardProjects({ image, caption, children }) {
         />
       )}
       <div className="p-6 flex flex-col flex-grow">
+        {title && <h3 className="text-lg font-bold text-gray-800">{title}</h3>}
+        {description && (
+          <p
+            className="my-6 text-lg dark:text-gray-500"
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(description),
+            }}
+          ></p>
+        )}
         {caption && (
-          <h3 className="text-lg font-semibold text-gray-800">{caption}</h3>
+          <div className="flex items-start gap-x-2 text-lg text-gray-800 mb-4">
+            <span className="font-semibold whitespace-nowrap">Built with:</span>
+            <span
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(caption),
+              }}
+            />
+          </div>
         )}
         <div className="mt-auto text-right">{children}</div>
       </div>
@@ -21,6 +44,8 @@ export default function CardProjects({ image, caption, children }) {
 }
 
 CardProjects.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
   caption: PropTypes.string,
   image: PropTypes.string,
   children: PropTypes.node,
