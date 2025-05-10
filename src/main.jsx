977cac9b1f7be40cbc34f4489f73bb02d840988c
Dwi <98dwiyulianto@gmail.com>
2025-05-10 18:34:08 +0700

    update index.jsx route (4)

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "../src/assets/admin/css/style.css";
 import "../src/assets/admin/css/satoshi.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
