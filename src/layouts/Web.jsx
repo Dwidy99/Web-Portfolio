a7132a3cfde24c4674e5face0e26457a8bbc9963
Dwi9 <98dwiyulianto@gmail.com>
2025-05-10 14:37:52 +0700

    Add no-Copy Feature

--- src/layouts/Web.jsx
+++ src/layouts/Web.jsx
@@ -8,15 +8,28 @@ import "../assets/web/js/script.js";
 import Navbar from "../components/web/Navbar.jsx";
 import Footer from "../components/web/Footer.jsx";
 import SnowEffect from "../components/general/SnowEffect.jsx";
+import PropTypes from "prop-types";
 
 export default function Web({ children }) {
   return (
-    <>
+    <div
+      style={{
+        userSelect: "none",
+        WebkitUserSelect: "none",
+        MozUserSelect: "none",
+        msUserSelect: "none",
+      }}
+      onContextMenu={(e) => e.preventDefault()}
+    >
       <Navbar />
-      {/* Gunakan komponen SnowEffect di sini */}
       <SnowEffect />
-      {children ? children : <p>Children are missing</p>}
+      {children ?? <p>Children are missing</p>}
       <Footer />
-    </>
+    </div>
   );
 }
+
+// Tambahkan propTypes untuk validasi props di JavaScript
+Web.propTypes = {
+  children: PropTypes.node,
+};
