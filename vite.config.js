cf32257285d0411906b8ec94305cefcbf227e7b2
Dwi <98dwiyulianto@gmail.com>
2025-10-24 17:02:56 +0700

    Deployment : vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,20 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": {},
+  build: {
+    chunkSizeWarningLimit: 3000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
   },
 });
